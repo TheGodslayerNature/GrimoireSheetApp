007b4489ce4c2e95b6b6fa93606308dd8bcd6d50
Alexandre Da Silva Alves <asalves@sefaz.al.gov.br>
2023-10-03 13:04:22 -0300

    Creating all arcanas

--- src/components/character/Arcanas.ts
+++ src/components/character/Arcanas.ts
@@ -1,26 +1,48 @@
 import { ArcanaSkills } from "./util/ArcanaSkills";
+
 export class Arcanas {
-    public static JOKER = new Arcanas("Joker",ArcanaSkills.JOKER_COMBAT_SKILL, ArcanaSkills.JOKER_OUT_OF_COMBAT_SKILL);
-    public static MAGE = new Arcanas("","","");
-    public static PRIESTESS = new Arcanas("","","");
-    public static EMPRESS = new Arcanas("","","");
+    public static JOKER = new Arcanas("Fool",ArcanaSkills.FOOL_COMBAT_SKILL, ArcanaSkills.FOOL_OUT_OF_COMBAT_SKILL);
+
+    public static MAGE = new Arcanas("Mage",ArcanaSkills.MAGE_COMBAT_SKILL,ArcanaSkills.MAGE_OUT_OF_COMBAT_SKILL);
+    
+    public static PRIESTESS = new Arcanas("Priestess",ArcanaSkills.PRIESTESS_COMBAT_SKILL,ArcanaSkills.PRIESTESS_OUT_OF_COMBAT_SKILL);
+    
+    public static EMPRESS = new Arcanas("Empress",ArcanaSkills.EMPRESS_COMBAT_SKILL,ArcanaSkills.EMPRESS_OUT_OF_COMBAT_SKILL);
+    
     public static EMPEROR = new Arcanas("Emperor", ArcanaSkills.EMPEROR_COMBAT_SKILL,ArcanaSkills.EMPEROR_OUT_OF_COMBAT_SKILL);    
-    public static HIEROPHANT = new Arcanas("","","")
-    public static LOVERS = new Arcanas("","","")
-    public static CHARIOT = new Arcanas("","","")
-    public static JUSTICE = new Arcanas("","","")
-    public static HERMIT = new Arcanas("","","")
-    public static FORTUNE = new Arcanas("","","")
-    public static STRENGTH = new Arcanas("","","")
-    public static HANGED = new Arcanas("","","")
-    public static DEATH = new Arcanas("","","")
-    public static TEMPERANCE = new Arcanas("","","")
-    public static DEVIL = new Arcanas("","","")
-    public static TOWER = new Arcanas("","","")
-    public static STAR = new Arcanas("","","")
-    public static MOON = new Arcanas("","","")
-    public static SUN = new Arcanas("","","")
-    public static JUDGMENT = new Arcanas("","","")
+    
+    public static HIEROPHANT = new Arcanas("Hierophant",ArcanaSkills.HIEROPHANT_COMBAT_SKILL,ArcanaSkills.HIEROPHANT_OUT_OF_COMBAT_SKILL)
+    
+    public static LOVERS = new Arcanas("Lovers",ArcanaSkills.LOVERS_COMBAT_SKILL,ArcanaSkills.LOVERS_OUT_OF_COMBAT)
+    
+    public static CHARIOT = new Arcanas("Chariot",ArcanaSkills.CHARIOT_COMBAT_SKILL,ArcanaSkills.CHARIOT_OUT_OF_COMBAT_SKILL)
+    
+    public static JUSTICE = new Arcanas("Justice",ArcanaSkills.JUSTICE_COMBAT_SKILL,ArcanaSkills.JUSTICE_COMBAT_SKILL)
+    
+    public static HERMIT = new Arcanas("Hermit",ArcanaSkills.HERMIT_COMBAT_SKILL,ArcanaSkills.HERMIT_OUT_OF_COMBAT_SKILL)
+    
+    public static FORTUNE = new Arcanas("Fortune",ArcanaSkills.FORTUNE_COMBAT_SKILL,ArcanaSkills.FORTUNE_OUT_OF_COMBAT_SKILL)
+    
+    public static STRENGTH = new Arcanas("Strength",ArcanaSkills.STRENGTH_COMBAT_SKILL,ArcanaSkills.STRENGTH_OUT_OF_COMBAT_SKILL)
+    
+    public static HANGED = new Arcanas("Hanged",ArcanaSkills.HANGED_COMBAT_SKILL,ArcanaSkills.HANGED_OUT_OF_COMBAT_SKILL)
+    
+    public static DEATH = new Arcanas("Death",ArcanaSkills.DEATH_COMBAT_SKILL,ArcanaSkills.DEATH_OUT_OF_COMBAT_SKILL)
+    
+    public static TEMPERANCE = new Arcanas("Temperance",ArcanaSkills.TEMPERANCE_COMBAT_SKILL,ArcanaSkills.TEMPERANCE_OUT_OF_COMBAT_SKILL)
+    
+    public static DEVIL = new Arcanas("Devil",ArcanaSkills.DEVIL_COMBAT_SKILL,ArcanaSkills.DEVIL_OUT_COMBAT_SKILL)
+    
+    public static TOWER = new Arcanas("Tower",ArcanaSkills.TOWER_COMBAT_SKILL,ArcanaSkills.TOWER_OUT_OF_COMBAT_SKILL)
+    
+    public static STAR = new Arcanas("Star",ArcanaSkills.STAR_COMBAT_SKILL,ArcanaSkills.STAR_OUT_OF_COMBAT_SKILL)
+    
+    public static MOON = new Arcanas("Moon",ArcanaSkills.MOON_COMBAT_SKILL,ArcanaSkills.MOON_OUT_OF_COMBAT_SKILL)
+    
+    public static SUN = new Arcanas("Sun",ArcanaSkills.SUN_COMBAT_SKILL,ArcanaSkills.SUN_OUT_OF_COMBAT_SKILL)
+    
+    public static JUDGMENT = new Arcanas("Judment",ArcanaSkills.JUDGMENT_COMBAT_SKILL,ArcanaSkills.JUDGMENT_OUT_COMBAT_SKILL)
+    
     public static WORLD = new Arcanas("","","")
     
     constructor(arcanaName:String ,combatEffect:String, outOfCombat: String){
--- src/components/character/util/ArcanaSkills.ts
+++ src/components/character/util/ArcanaSkills.ts
@@ -1,7 +1,69 @@
 export class ArcanaSkills {
-    static JOKER_COMBAT_SKILL:String = "Durante o cálculo de iniciativa, você é automaticamente o primeiro e todos os seus aliados recebem +10 nos seus testes de iniciativa.";
-    static JOKER_OUT_OF_COMBAT_SKILL:String = "Adicione 1d12 a qualquer habilidade de qualquer Usuário até o final da cena.";
 
-    static EMPEROR_COMBAT_SKILL = "";
-    static EMPEROR_OUT_OF_COMBAT_SKILL = "";
+    static FOOL_COMBAT_SKILL:String = "Durante o cálculo de iniciativa, você é automaticamente o primeiro e todos os seus aliados recebem +10 nos seus testes de iniciativa.";
+    static FOOL_OUT_OF_COMBAT_SKILL:String = "Adicione 1d12 a qualquer habilidade de qualquer Usuário até o final da cena.";
+
+    static MAGE_COMBAT_SKILL:String = "Você se torna imune a Status prejudiciais até o final do combate.";
+    static MAGE_OUT_OF_COMBAT_SKILL:String = "Você pode usar sua pontuação de Disciplina ao invés de qualquer outra Habilidade Social até o final da cena.";
+
+    //Adicionar as tipagens nas skills quando estiverem todas 
+
+    static PRIESTESS_COMBAT_SKILL = "Até o final do combate, você e todos seus aliados podem trocar de Persona como uma ação menor.";
+    static PRIESTESS_OUT_OF_COMBAT_SKILL = "Você pode requisitar uma dica do Narrador sobre uma situação misteriosa ou um enigma.";
+
+    static EMPRESS_COMBAT_SKILL = "Um inimigo sofre: Chance de Charme: 80%";
+    static EMPRESS_OUT_OF_COMBAT_SKILL = "Um item qualquer recebe um desconto de até 2 PR (isso não pode reduzir o preço do item para menos de 0.25 PR) até o final da cena.";
+
+    static EMPEROR_COMBAT_SKILL = "Você pode determinar os alvos de todos os ataques e magias e o deslocamento do alvo em magias de Linha até o começo do seu próximo turno.";
+    static EMPEROR_OUT_OF_COMBAT_SKILL = "Dobre o seu valor de Charme para um teste.";
+
+    static HIEROPHANT_COMBAT_SKILL = "Você e todos os seus aliados são imunes aos efeitos de Medo e Pânico até o final do combate.";
+    static HIEROPHANT_OUT_OF_COMBAT_SKILL = "Dobre seu Charme para o propósito de convencer alvos a se conformarem com alguma situação até o final da cena.";
+
+    static LOVERS_COMBAT_SKILL = "Um alvo que você pode ver é forçado a escolher entre um dos efeitos abaixo: ❖ -1 HDC ❖ -1 DDC Esse efeito dura até o final do combate e é tratado como um Debuff.";
+    static LOVERS_OUT_OF_COMBAT = "Dobre sua Expressão para interagir com estranhos até o final da cena.";
+
+    static CHARIOT_COMBAT_SKILL = "Você e todos os seus aliados se recuperam de todos os status prejudiciais, recebem VITd4 PV e podem realizar uma ação padrão imediatamente.";
+    static CHARIOT_OUT_OF_COMBAT_SKILL = "Dobre sua Disciplina para um teste.";
+
+    static JUSTICE_COMBAT_SKILL = "Quando você sofrer dano de qualquer fonte, cause a mesma quantidade de dano ao causador. Esse dano é do Tipo Intel.";
+    static JUSTICE_OUT_OF_COMBAT_SKILL = "Você detecta mentiras sem falha até o final da cena. O Narrador pode optar por te dar 1 Ponto de Aspecto para não revelar a mentira em alguma interação.";
+
+    static HERMIT_COMBAT_SKILL = "Você não pode ser escolhido como alvo de um ataque. Esse efeito dura até o final do combate ou até você declarar um ataque.";
+    static HERMIT_OUT_OF_COMBAT_SKILL = "Você pode somar 2d12 a qualquer teste que deve ser realizado sozinho."
+
+    static FORTUNE_COMBAT_SKILL = "Você e todos os seus aliados ganham 5 Cargas de Sorte e Margem de Crítico +1 até o final do combate.";
+    static FORTUNE_OUT_OF_COMBAT_SKILL = "Após ativar essa Propagação, até o fim do dia, depois de Arriscar um dado, você pode gastar 1 Carga de Sorte para rolar a mesma categoria de dado que você arriscou: Adicione o resultado da rolagem ao resultado final do teste.";
+
+    static STRENGTH_COMBAT_SKILL = "Você pode iniciar negociação com uma Sombra imediatamente, ignorando as condições normais de negociação.";
+    static STRENGTH_OUT_OF_COMBAT_SKILL = "Dobre sua Coragem para um teste.";
+
+    static HANGED_COMBAT_SKILL = "Você pode gastar o uso de uma magia do seu deck para conjurar qualquer outra magia de Tier igual ou menor, inclusive magias fora do seu deck.";
+    static HANGED_OUT_OF_COMBAT_SKILL = "Força um NPC a seguir uma ordem específica pelos próximos 1d6 minutos. O Narrador pode escolher fazer o NPC resistir a ordem, em qualquer capacidade, pagando 1 Ponto de Aspecto ao Usuário que ativou esse efeito.";
+
+    static DEATH_COMBAT_SKILL = "Até o final do combate, caso você entre em Portas da Morte, você permanece capaz de agir normalmente, mas não pode invocar sua Persona.";
+    static DEATH_OUT_OF_COMBAT_SKILL = "Adicione 2d12 a um teste para algo que você nunca tenha feito antes.";
+
+    static TEMPERANCE_COMBAT_SKILL = "Soma o PV atual de qualquer número de alvos e redistribui igualmente para todos, contanto que a quantidade de PV de cada alvo não exceda seu PV máximo. Caso isso ocorra, todos os alvos ficam com o PV igual ao maior PV possível e pontos extras são desconsiderados.";
+    static TEMPERANCE_OUT_OF_COMBAT_SKILL = "Receba uma dica sobre o propósito, aspiração ou objetivo de um NPC.";
+
+    static DEVIL_COMBAT_SKILL = "O alvo não pode se deixar sua posição atual por 1d6+1 turnos.";
+    static DEVIL_OUT_COMBAT_SKILL = "Dobre seu Charme para testes que envolvam sedução até o final da cena.";
+
+    static TOWER_COMBAT_SKILL = "Escolha, para qualquer número de alvos que você possa ver (escolhido individualmente para cada alvo: ❖ Todos os efeitos de Buff se tornam o efeito de Debuff correspondente até o final do efeito original; ❖ Todos os efeitos de Debuff se tornam o efeito de Buff correspondente até o final do efeito original;";
+    static TOWER_OUT_OF_COMBAT_SKILL = "Um NPC a sua escolha é forçado a falar a verdade. Caso ele insista em mentir, você recebe 1 Ponto de Aspecto.";
+
+    static STAR_COMBAT_SKILL = "Qualquer número de alvos não pode realizar qualquer ação até o final do turno do alvo.";
+    static STAR_OUT_OF_COMBAT_SKILL = "Dobre sua Empatia para aplacar, acalmar ou apaziguar até o final da cena.";
+
+    static MOON_COMBAT_SKILL = "Qualquer número de alvos sofre: Chance de Medo: 80% A chance de recuperação é 33% mas deve ser rolada no final dos seus turnos ao invés dos turnos dos alvos.";
+    static MOON_OUT_OF_COMBAT_SKILL = "Ganhe imediatamente uma dica sobre a motivação, objetivos ou sinceridade de um NPC.";
+
+    static SUN_COMBAT_SKILL = "Qualquer número de alvos recupera 100% PV.";
+    static SUN_OUT_OF_COMBAT_SKILL = "Dobre sua Expressão ou Charme, à sua escolha, para entreter ou divertir.";
+    
+    static JUDGMENT_COMBAT_SKILL = "Um alvo que você possa a ver é revivido com 100% PV";
+    static JUDGMENT_OUT_COMBAT_SKILL = "Escolha um NPC na cena. O Narrador deve informar ao grupo se a motivação desse personagem é mais próxima de uma das opções abaixo: ❖ “Verdadeiramente Boa” ❖ “Viciosamente Má” ❖ “Perfeitamente Cinza”";
+
+
 }
